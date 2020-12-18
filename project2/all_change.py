fd916fb7488eba9d000206abf34e7bbe6234912b
BuildTools <unconfigured@null.spigotmc.org>
2020-12-18 23:09:47 +0100

    bottom up approach

--- project2/all_change.py
+++ project2/all_change.py
@@ -94,6 +94,37 @@ def all_change_number_only(cur: Currency, r: float):
     return T[-1][-1]
 
 
+def all_change_number_only_bottom_up(cur: Currency, r: float):
+    """This function, given in input a Currency Object and a float number r with at most two decimal points, returns all
+    possible way to change r value with the currency c.
+    (E.g., for a currency whose denominations are {0.1, 0.2, 0.5} and r=0.6, the algorithm must return 5, and the five
+    different possible ways that can be used for changing 0.6, i.e., 0.5+0.1, 3*0.2, 0.2+4*0.1, 6*0.1.)
+
+    :param cur: the currency to use to get the change;
+    :param r: the value to return the change;
+    :return: the number of different ways that value r can be achieved by using denominations of the given currency;"""
+
+    # introducing convenient notation working in cents
+    m = cur.num_denominations()
+    S = [_float2int(x) for x in cur.iter_denominations()]
+    n = _float2int(r)
+
+    # Construct table
+    T = [0 for _ in range(n + 1)]
+
+    # Base case (value zero has just one solution: empty list)
+    T[0] = 1
+
+    # Pick all coins one by one and update the T[] values
+    # after the index greater than or equal to the value of the
+    # picked coin
+    for i in range(0, m):
+        for j in range(S[i], n + 1):
+            T[j] += T[j - S[i]]
+
+    return T[n]
+
+
 if __name__ == '__main__':
     c = Currency('EUR')
     c.add_denomination(0.01)
@@ -105,5 +136,6 @@ if __name__ == '__main__':
     c.add_denomination(1)
     c.add_denomination(2)
 
-    print(all_change_number_only(c, 30))
+    print(all_change_number_only(c, 0.04))
+    print(all_change_number_only_bottom_up(c, 0.04))
     # print(all_change(c, 2))
