160a1b0c0018b1ee38ed61cc085a077611ac74c6
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 00:22:42 +0100

    fixed return cycle repr bug. Added docs with image

--- project2/negative_cycle_detector.py
+++ project2/negative_cycle_detector.py
@@ -58,16 +58,25 @@ def _reconstruct_cycle(g, s, V, P):
     if s in cycle:
         # if starting currency is in the cycle
         i = cycle.index(s)
-        return cycle[i:] + cycle[1:i + 1], earn
+        return cycle[i:] + cycle[1:i + 1], round(earn, 10)
     else:
         # if starting currency is not in the cycle
         earn_go_to_cycle = g.get_edge(V[s.code()], V[cycle[0].code()]).element()
         earn_go_back = g.get_edge(V[cycle[-1].code()], V[s.code()]).element()
         move_earn = earn_go_back + earn_go_to_cycle
+
         k = 0
         while move_earn + k * earn > 0:
             k += 1
-        return [s] + k * cycle + [s], k * earn + earn_go_to_cycle + earn_go_back
+
+        new_cycle = [s]
+        if k > 1:
+            new_cycle += (k-1) * cycle[:-1]
+            new_cycle += cycle
+        else:
+            new_cycle += (k * cycle)
+        new_cycle.append(s)
+        return new_cycle, round(k * earn + earn_go_to_cycle + earn_go_back, 10)
 
 
 def arbitrage_opportunity(C, s):
@@ -83,7 +92,10 @@ def arbitrage_opportunity(C, s):
     g, V = _create_graph(C)  # V[currencycode] = vertex(currency)
 
     # check if s is in C
-    if V[s.code()] not in g.vertices():
+    try:
+        if V[s.code()] not in g.vertices():
+            return None
+    except KeyError:
         return None
 
     D = {}  # Store D[vertex] = w max
@@ -117,17 +129,100 @@ def arbitrage_opportunity(C, s):
 
 
 if __name__ == '__main__':
+    iteration = 5
+
+    # GRAPH 1 in ./arbitrage_opportunity_img/graph1.jpg
     eur = Currency('EUR')
     eur.add_change('GBP', -0.31)
     eur.add_change('USD', 0.1)
 
     usd = Currency('USD')
-    usd.add_change('EUR', 0.005)
+    usd.add_change('EUR', 0.1)
     usd.add_change('GBP', 0.1)
 
     gbp = Currency('GBP')
     gbp.add_change('USD', 0.1)
     gbp.add_change('EUR', 0.1)
 
-    for _ in range(1000):
+    print('\n\n---------- GRAPH 1 - STARTING FROM EUR ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp}, eur))
+
+    print('\n\n---------- GRAPH 1 - STARTING FROM GBP ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp}, gbp))
+
+    print('\n\n---------- GRAPH 1 - STARTING FROM GBP ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp}, usd))
+
+    aed = Currency('AED')
+    aed.add_change('USD', -0.1)
+    aed.add_change('EUR', 0.2)
+    aed.add_change('GBP', 0.23)
+
+    print('\n\n---------- GRAPH 1 - STARTING FROM AED ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp}, aed))
+
+    # GRAPH 2 in ./arbitrage_opportunity_img/graph2.jpg
+    eur = Currency('EUR')
+    eur.add_change('GBP', -0.42)
+    eur.add_change('USD', 0.34)
+    eur.add_change('AED', 0.11)
+
+    usd = Currency('USD')
+    usd.add_change('EUR', 0.05)
+    usd.add_change('GBP', -0.05)
+    usd.add_change('AED', 0.43)
+
+    gbp = Currency('GBP')
+    gbp.add_change('USD', 0.33)
+    gbp.add_change('EUR', 0.2)
+    gbp.add_change('AED', 0.15)
+
+    aed = Currency('AED')
+    aed.add_change('USD', -0.1)
+    aed.add_change('EUR', 0.2)
+    aed.add_change('GBP', 0.23)
+
+    print('\n\n---------- GRAPH 2 - STARTING FROM EUR ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp, aed}, eur))
+
+    print('\n\n---------- GRAPH 2 - STARTING FROM USD ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp, aed}, usd))
+
+    print('\n\n---------- GRAPH 2 - STARTING FROM AED ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp, aed}, aed))
+
+    print('\n\n---------- GRAPH 2 - STARTING FROM GBP ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp, aed}, gbp))
+
+    # GRAPH 3 in ./arbitrage_opportunity_img/graph3.jpg
+    eur = Currency('EUR')
+    eur.add_change('GBP', -0.05)
+    eur.add_change('USD', -0.1)
+
+    usd = Currency('USD')
+    usd.add_change('EUR', 0.1)
+    usd.add_change('GBP', 0.1)
+
+    gbp = Currency('GBP')
+    gbp.add_change('USD', 0.1)
+    gbp.add_change('EUR', 0.1)
+
+    print('\n\n---------- GRAPH 3 - STARTING FROM EUR ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp}, eur))
+
+    print('\n\n---------- GRAPH 3 - STARTING FROM GBP ----------------')
+    for _ in range(iteration):
+        print(arbitrage_opportunity({eur, usd, gbp}, gbp))
+
+    print('\n\n---------- GRAPH 3 - STARTING FROM GBP ----------------')
+    for _ in range(iteration):
         print(arbitrage_opportunity({eur, usd, gbp}, usd))
