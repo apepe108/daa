456ea88bc413c1a25d343fabfb7d4f6d395f9572
Alessio Pepe <a.pepe108@studenti.unisa.it>
2020-12-22 17:23:24 +0100

    add interface

---
+++ project2/travelling-salesman.py
@@ -0,0 +1,7 @@
+from project1.currency import Currency
+
+
+def excange_tour(C):
+    """Design a local search algorithm that takes in input a set of Currency objects and looks for
+    an exchange tour of minimal rate."""
+    pass
