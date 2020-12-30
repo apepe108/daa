7a91307b46b00c5e601d7f63dba230867c47ef25
Alessio Pepe <a.pepe108@studenti.unisa.it>
2020-12-30 13:48:31 +0100

    all implemented

--- project2/kc1c2_experimental_valutation.py
+++ project2/kc1c2_experimental_valutation.py
@@ -4,6 +4,10 @@ from project1.abtree.abtree_map import ABTreeMap
 from project1.currency import Currency
 from project2 import kc1c2_coverage
 
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import numpy as np
+
 
 def opt_search(T, k, c1, c2):
     """ Algorithm that to compute the (k, C1, C2)-cover of T with the minimum number of nodes. The algorithm must
@@ -58,35 +62,90 @@ def _by_key(e):
     return e[0]
 
 
-def _montecarlo(T, c1, c2, k, n_try=1000):
-    e_min = float('+inf')
-    e_max = float('-inf')
-    e_avg = 0
+def montecarlo_on_k(L, c1, c2, k1, k2, n_try):
+    e_res = []
+    o_res = []
 
-    o_min = float('+inf')
-    o_max = float('-inf')
-    o_avg = 0
+    for i in range(k1, k2 + 1):
+        print(i, 'of', k2)
 
-    for i in range(n_try):
-        o_sol = opt_search(T, k, c1, c2)
-        e_sol = kc1c2_coverage.search(T, k, c1, c2)
+        e_min, e_max, e_avg = float('inf'), float('-inf'), 0
+        o_min, o_max, o_avg = float('inf'), float('-inf'), 0
 
-        if len(o_sol) < o_min:
-            o_min = len(o_sol)
-        if len(o_sol) > o_max:
-            o_max = len(o_sol)
-        o_avg += len(o_sol)
+        for j in range(n_try):
+            # generate random tree
+            t = ABTreeMap(2, 8)
+            random.shuffle(L)
+            for cur_code in L:
+                t[cur_code] = Currency(cur_code)
 
-        if len(e_sol) < e_min:
-            e_min = len(e_sol)
-        if len(e_sol) > e_max:
-            e_max = len(e_sol)
-        e_avg += len(e_sol)
+            # compute max, min and avg solution for greedy algorithm
+            res = kc1c2_coverage.search(t, i, c1, c2)
+            if len(res) < e_min:
+                e_min = len(res)
+            if len(res) > e_max:
+                e_max = len(res)
+            e_avg += len(res)
 
-    o_avg = o_avg / n_try
-    e_avg = e_avg / n_try
+            # compute max, min and avg solution for optimal algorithm
+            res = opt_search(t, i, c1, c2)
+            if len(res) < o_min:
+                o_min = len(res)
+            if len(res) > o_max:
+                o_max = len(res)
+            o_avg += len(res)
 
-    return o_min, o_max, o_avg, e_min, e_max, e_avg
+        e_avg /= n_try
+        o_avg /= n_try
+
+        e_res.append((e_min, e_max, e_avg))
+        o_res.append((o_min, o_max, o_avg))
+
+    e_min, e_max, e_avg = zip(*e_res)
+    o_min, o_max, o_avg = zip(*o_res)
+    e_bound = [e / o for e, o in zip(*(e_max, o_max))]
+    print(e_bound)
+
+    _plot_result(e_min, e_max, e_avg, o_min, o_max, o_avg, e_bound, k1, k2 + 1)
+
+
+def _plot_result(e_min, e_max, e_avg, o_min, o_max, o_avg, e_bound, k1, k2):
+    gs = gridspec.GridSpec(3, 1)
+
+    plt.figure()
+
+    plt.suptitle('2-8 Tree with 50 element in [c1, c2]')
+
+    plt.subplot(gs[0, 0])
+    plt.gca().set_title('greedy approach - optimal approach - theoretical/experimental bound', fontsize=10)
+    plt.ylabel('#node')
+    plt.plot(list(range(k1, k2)), e_min, 'b--', label='min')
+    plt.plot(list(range(k1, k2)), e_max, 'r--', label='max')
+    plt.plot(list(range(k1, k2)), e_avg, 'g', label='avg')
+    # Place a legend above this subplot, expanding itself to fully use the given bounding box.
+    plt.legend(fontsize=7)
+
+    plt.subplot(gs[1, 0])
+    plt.ylabel('#node')
+    plt.plot(list(range(k1, k2)), o_min, 'b--', label='min')
+    plt.plot(list(range(k1, k2)), o_max, 'r--', label='max')
+    plt.plot(list(range(k1, k2)), o_avg, 'g', label='avg')
+    # Place a legend above this subplot, expanding itself to fully use the given bounding box.
+    plt.legend(fontsize=7)
+
+    x = np.arange(k1, k2, 1)
+    y = x / (x // 7 + 1)
+
+    plt.subplot(gs[2, 0])
+    plt.xlabel('k')
+    plt.ylabel('alpha')
+    plt.plot(x, y, 'r', label='theoretical')
+    plt.axhline(y=1, color='k', linestyle='--', label='optimal')
+    plt.plot(list(range(k1, k2)), e_bound, 'g', label='experimental')
+    # Place a legend above this subplot, expanding itself to fully use the given bounding box.
+    plt.legend(fontsize=7)
+
+    plt.savefig('exp.png')
 
 
 if __name__ == '__main__':
@@ -103,22 +162,8 @@ if __name__ == '__main__':
          'XAU', 'XBA', 'XBB', 'XBC', 'XBD', 'XCD', 'XDR', 'XOF', 'XPD', 'XPF', 'XPT', 'XSU', 'XTS', 'XUA', 'XXX', 'YER',
          'ZAR', 'ZMW', 'ZWL']
 
-    i1 = random.randint(0, len(L) - 1)
-    i2 = random.randint(i1, len(L) - 1)
-    c1 = L[i1]
-    c2 = L[i2]
+    k = 50
+    c1 = 'IDR'
+    c2 = 'PHP'
 
-    random.shuffle(L)
-
-    t = ABTreeMap(2, 8)
-    for cur_code in L:
-        t[cur_code] = Currency(cur_code)
-    print(t)
-
-    k = random.randint(0, len(L) // 3)
-
-    print('opt_search(t, {}, {}, {}) -> '.format(k, c1, c2), end='')
-    print(opt_search(t, k, c1, c2))
-
-    print('search(t, {}, {}, {}) -> '.format(k, c1, c2), end='')
-    print(kc1c2_coverage.search(t, k, c1, c2))
+    montecarlo_on_k(L, c1, c2, 0, k, 10000)
